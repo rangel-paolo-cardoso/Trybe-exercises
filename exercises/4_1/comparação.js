2a140ccd66426f2ef261a0ca68ff57c3061761f2
Rangel Paolo <paolorangel1996@outlook.com>
2020-03-26 18:13:46 -0300

    Construída sua lógica de comparação de valores.

--- exercises/4_1/comparação.js
+++ exercises/4_1/comparação.js
@@ -2,5 +2,7 @@ let number1 = 10;
 let number2 = 8;
 
 if(number1 > number2) {
-    console.log()
+    console.log('O maior número é ', number1);
+} else {
+    console.log('O maior número é ', number2);
 }

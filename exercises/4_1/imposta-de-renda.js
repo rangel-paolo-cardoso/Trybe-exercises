1b36d3e069a5e1cdf4f66374ef6313368e65441f
Rangel Paolo <paolorangel1996@outlook.com>
2020-03-26 20:07:48 -0300

    Terminado o cálculo do salário líquido.

--- exercises/4_1/imposta-de-renda.js
+++ exercises/4_1/imposta-de-renda.js
@@ -1,4 +1,6 @@
 let salariobruto = 3000;
+let salarioBase;
+let salarioLiquido;
 let inss;
 let ir;
 
@@ -8,18 +10,23 @@ if(salariobruto <= 1556.94) {
     inss = salariobruto * 0.09;
 } else if(salariobruto >= 2594.93 && salariobruto <= 5189.82) {
     inss = salariobruto * 0.11;
-} else (salariobruto > 5189.82) {
+} else  {
     inss = 570.88;
 }
 
-if(salariobruto <= 1903.98) {
+salarioBase = salariobruto - inss;
+
+if(salarioBase <= 1903.98) {
     ir = 0;
-} else if(salariobruto >= 1903.99 && salariobruto <= 2826.65) {
-    ir = (salariobruto * 0.09) - 142.8;
-} else if(salariobruto >= 2826.66 && salariobruto <= 3751.05) {
-    ir = (salariobruto * 0.15) - 354.8;
-} else if(salariobruto >= 3751.06 && salariobruto <= 4664.68) {
-    ir = (salariobruto * 0.225) - 636.13;
+} else if(salarioBase >= 1903.99 && salarioBase <= 2826.65) {
+    ir = (salarioBase * 0.075) - 142.8;
+} else if(salarioBase >= 2826.66 && salarioBase <= 3751.05) {
+    ir = (salarioBase * 0.15) - 354.8;
+} else if(salarioBase >= 3751.06 && salarioBase <= 4664.68) {
+    ir = (salarioBase * 0.225) - 636.13;
 } else {
-    ir = (salariobruto * 0.275) - 869.36;
+    ir = (salarioBase * 0.275) - 869.36;
 }
+
+salarioLiquido = salarioBase - ir;
+console.log('Salário líquido é de: R$', salarioLiquido)

3c852ff3477193209caa8ecd5e9b8bef76b16589
Rangel Paolo <paolorangel1996@outlook.com>
2020-03-30 15:15:41 -0300

    Adicionada função angulosDoTriangulo.js

--- exercises/4_1/todas-as-funcoes.js
+++ exercises/4_1/todas-as-funcoes.js
@@ -13,3 +13,11 @@ function adicao(a, b) {
 function adicao(a, b) {
     return `O resultado de ${a} / ${b} = ${a / b}`;
 }
+
+function angulosDoTriangulo(ang1, ang2, ang3) {
+    let result = false;
+    if(ang1 + ang2 + ang3 === 180){
+        result = true;
+    }
+    return `Os ângulos representam os ângulo internos de um triângulo? ${result}`;
+}

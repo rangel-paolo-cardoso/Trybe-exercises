f5c3ac9b3fffe84ab7e0398c40041178127f5666
Rangel Paolo <paolorangel1996@outlook.com>
2020-03-30 15:31:59 -0300

    Adicionada função impostoDeRenda.js

--- exercises/4_1/todas-as-funcoes.js
+++ exercises/4_1/todas-as-funcoes.js
@@ -47,4 +47,38 @@ function imparDeTres(num1, num2, num3) {
     }
 }
 
-//console.log(imparDeTres(12, 23, 22));
+function impostoDeRenda(salBruto) {
+    let salarioBase;
+    let salarioLiquido;
+    let inss;
+    let ir;
+
+    if(salBruto <= 1556.94) {
+        inss = salBruto * 0.08;
+    } else if(salBruto >= 1556.95 && salBruto <= 2594.92) {
+        inss = salBruto * 0.09;
+    } else if(salBruto >= 2594.93 && salBruto <= 5189.82) {
+        inss = salBruto * 0.11;
+    } else  {
+        inss = 570.88;
+    }
+
+    salarioBase = salBruto - inss;
+
+    if(salarioBase <= 1903.98) {
+        ir = 0;
+    } else if(salarioBase >= 1903.99 && salarioBase <= 2826.65) {
+        ir = (salarioBase * 0.075) - 142.8;
+    } else if(salarioBase >= 2826.66 && salarioBase <= 3751.05) {
+        ir = (salarioBase * 0.15) - 354.8;
+    } else if(salarioBase >= 3751.06 && salarioBase <= 4664.68) {
+        ir = (salarioBase * 0.225) - 636.13;
+    } else {
+        ir = (salarioBase * 0.275) - 869.36;
+    }
+
+    salarioLiquido = salarioBase - ir;
+    return `Salário líquido é de R$ ${salarioLiquido}.`
+}
+
+console.log(impostoDeRenda(3000));

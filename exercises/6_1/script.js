dd638baf127f980d4b286b16b85ee0fdaf6387a4
Rangel Paolo <paolorangel1996@outlook.com>
2020-04-15 19:21:18 -0300

    Gerando select automaticamente com JavaScript.

---
+++ exercises/6_1/script.js
@@ -0,0 +1,42 @@
+const objEstados = {
+    'ac': 'Acre',
+    'al': 'Alagoas',
+    'ap': 'Amapá',
+    'am':'Amazonas',
+    'ba': 'Bahia',
+    'ce': 'Ceará',
+    'df': 'Distrito Federal',
+    'es': 'Espírito Santo',
+    'go': 'Goiás',
+    'ma': 'Maranhão',
+    'mt': 'Mato Grosso',
+    'ms': 'Mato Grosso do Sul',
+    'mg': 'Minas Gerais',
+    'pa': 'Pará',
+    'pb': 'Paraíba',
+    'pr': 'Paraná',
+    'pe': 'Pernambuco',
+    'pi': 'Piauí',
+    'rj': 'Rio de Janeiro',
+    'rn': 'Rio Grande do Norte',
+    'rs': 'Rio Grande do Sul',
+    'ro': 'Rondônia',
+    'rr': 'Roraima',
+    'sc': 'Santa Catarina',
+    'sp': 'São Paulo',
+    'se': 'Sergipe',
+    'to': 'Tocantins',
+};
+
+function setEstate() {
+    const estados = document.getElementById('cEst');
+    for (estado in objEstados) {
+        const option = document.createElement('option');
+        option.innerText = objEstados[estado];   
+        estados.appendChild(option);
+    }
+}
+
+window.onload = function () {
+    setEstate();
+};

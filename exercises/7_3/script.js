6c0727a9f82aa98ec307b0eba4fc8390b054af9e
Rangel Paolo <paolorangel1996@outlook.com>
2020-04-28 21:22:56 -0300

    Pronto até que eu acrescente mais algo.

--- exercises/7_3/script.js
+++ exercises/7_3/script.js
@@ -1,4 +1,12 @@
 const grid = document.getElementById('grid');
+const efeitos = [
+    'grow',
+    'rotateLeft',
+    'rotateRight',
+    'rotateX',
+    'rotateY',
+    'translateIt'
+]; // Vetor de efeitos.
 const cartas = [
     'cartas/cinco-de-ouros.png',
     'cartas/nove-de-espadas.png',
@@ -6,14 +14,24 @@ const cartas = [
     'cartas/sete-de-espadas.png',
     'cartas/sete-de-paus.png',
     'cartas/tres-de-ouros.png'
-];
+]; // Vetor de endereços de cartas.
 
-function posCarta() {
+function posCarta() { // Posição da carta.
     return Math.floor(Math.random() * cartas.length);
 }
 
-function pegaCarta() {
+function posEfeito() { // Posição do efeito.
+    return Math.floor(Math.random() * efeitos.length);
+}
+
+function numbGerardor() { // Número de 0 a 10.
+    return Math.ceil(Math.random() * 3);
+}
+
+function pegaCarta() { // Insere cartas no grid.
     const img = document.createElement('img');
     img.src = cartas[posCarta()];
+    img.className = efeitos[posEfeito()];
+    img.style.transition = `all ${numbGerardor()}s`;
     grid.appendChild(img);
 }

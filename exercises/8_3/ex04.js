8ccee27f867be936a9c12774f5f38fca74328fc8
Rangel Paolo <paolorangel1996@outlook.com>
2020-05-05 21:18:33 -0300

    myRemove([1,2,3,4], 5) returning [1,2,3,4] it is Ok.

--- exercises/8_3/ex04.js
+++ exercises/8_3/ex04.js
@@ -13,4 +13,5 @@ const myArray = [1, 2, 3, 4];
 assert.deepEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
 assert.notDeepEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
 assert.deepEqual(myArray, [1, 2, 3, 4]);
+assert.deepEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
 // console.log(myRemove([1, 2, 3, 4], 3));

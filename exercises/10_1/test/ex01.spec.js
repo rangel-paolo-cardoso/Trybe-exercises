2e35030329fcde5a222b0b9a8296dd238ee9c329
Rangel Paolo <paolorangel1996@outlook.com>
2020-05-18 22:03:58 -0300

    Ex01 done 100%.

--- exercises/10_1/test/ex01.spec.js
+++ exercises/10_1/test/ex01.spec.js
@@ -3,6 +3,12 @@ const sum = require('../src/ex01');
 describe('Função soma', () => {
   test('Deve somar dois números', () => {
     expect(sum(4, 5)).toBe(9);
+    expect(sum(0, 0)).toBe(0);
+  });
+
+  test('Verifica se da Erro', () => {
+    expect(() => sum(4, '5')).toThrow();
+    expect(() => sum(4, '5')).toThrow('Parâmetros devem ser números!');
   });
 });
 

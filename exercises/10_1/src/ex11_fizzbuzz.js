965f3ea22000b8e4c10046c11b605fba6e3da1c3
Rangel Paolo <paolorangel1996@outlook.com>
2020-05-20 01:56:57 -0300

    Refatorado mais uma vez para diminuir o tamanho das funções.

--- exercises/10_1/src/ex11_fizzbuzz.js
+++ exercises/10_1/src/ex11_fizzbuzz.js
@@ -2,9 +2,7 @@ const fizzBuzz = (n) => {
   let i = 0;
   while (i < n) {
     i += 1;
-    if (typeof i !== 'number') {
-      console.log(false);
-    } else if (i % 3 === 0 && i % 5 === 0) {
+    if (i % 3 === 0 && i % 5 === 0) {
       console.log('fizzbuzz');
     } else if (i % 3 === 0) {
       console.log('fizz');

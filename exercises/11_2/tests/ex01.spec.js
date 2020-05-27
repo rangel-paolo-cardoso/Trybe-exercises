a45443be7229de0d17b64c7aa948d60f7664e477
Rangel Paolo <paolorangel1996@outlook.com>
2020-05-27 02:26:48 -0300

    Exercício 2 feito. Usando mockImplementation e mockReset.

--- exercises/11_2/tests/ex01.spec.js
+++ exercises/11_2/tests/ex01.spec.js
@@ -1,7 +1,7 @@
 const exs = require('../exercises/ex01');
 
 describe('Realizando Mocks', () => {
-  test('', () => {
+  test('Teste de chamada de randNum', () => {
     exs.randNum = jest.fn().mockReturnValue(10);
 
     exs.randNum();
@@ -9,5 +9,14 @@ describe('Realizando Mocks', () => {
     expect(exs.randNum()).toBe(10);
     expect(exs.randNum()).toBe(10);
     expect(exs.randNum).toHaveBeenCalledTimes(3);
+  });
+
+  test('Teste de mock implementation de randNum', () => {
+    exs.randNum.mockReset();
+    exs.randNum.mockImplementationOnce((a, b) => a / b);
+
+    expect(exs.randNum(20, 2)).toBe(10);
+    expect(exs.randNum).toHaveBeenCalled();
+    expect(exs.randNum).toHaveBeenCalledTimes(1);
   });
 });

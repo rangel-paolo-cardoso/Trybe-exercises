b10558f1433ba29c44b325b8e6d6232130ba4133
Rangel Paolo <paolorangel1996@outlook.com>
2020-05-27 13:46:02 -0300

    Exercício 4 feito.

--- exercises/11_2/tests/ex01.spec.js
+++ exercises/11_2/tests/ex01.spec.js
@@ -1,3 +1,4 @@
+// Os 3 primeiros exercícios.
 const exs = require('../exercises/ex01');
 
 describe('Realizando Mocks', () => {
@@ -39,5 +40,33 @@ describe('Realizando Mocks', () => {
     expect(exs.randNum).toHaveBeenCalledWith(20);
     expect(exs.randNum).toHaveBeenCalledWith(2);
     expect(exs.randNum).toHaveBeenCalledTimes(2);
+  });
+});
+
+// Exercício 4 em diante.
+const exs4 = require('../exercises/ex01_4');
+
+jest.mock('../exercises/ex01_4');
+
+describe('Mockagem de 3 funções de uma vez', () => {
+  test('Testa implementação de concat e to LowerCase', () => {
+    exs4.toUpper.mockImplementation(str => str.toLowerCase());
+    exs4.firstLetter.mockImplementation(str => str[str.length - 1]);
+    exs4.conca.mockImplementation((s1, s2, s3) => s1.concat(s2, s3));
+
+    expect(exs4.toUpper('Rangel')).toBe('rangel');
+    expect(exs4.toUpper).toHaveBeenCalled();
+    expect(exs4.toUpper).toHaveBeenCalledTimes(1);
+    expect(exs4.toUpper).toHaveBeenCalledWith('Rangel');
+
+    expect(exs4.firstLetter('Rangel')).toBe('l');
+    expect(exs4.firstLetter).toHaveBeenCalled();
+    expect(exs4.firstLetter).toHaveBeenCalledTimes(1);
+    expect(exs4.firstLetter).toHaveBeenCalledWith('Rangel');
+
+    expect(exs4.conca('Rangel', 'Paolo', 'Cardoso')).toBe('RangelPaoloCardoso');
+    expect(exs4.conca).toHaveBeenCalled();
+    expect(exs4.conca).toHaveBeenCalledTimes(1);
+    expect(exs4.conca).toHaveBeenCalledWith('Rangel', 'Paolo', 'Cardoso');
   });
 });

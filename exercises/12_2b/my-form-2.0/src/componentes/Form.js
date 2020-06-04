b4b3954825870208d68dcf21dd21d571aa481701
Rangel Paolo <paolorangel1996@outlook.com>
2020-06-03 21:58:28 -0300

    Formulário contruído.

---
+++ exercises/12_2b/my-form-2.0/src/componentes/Form.js
@@ -0,0 +1,69 @@
+import React, {Component} from 'react';
+import data from './data';
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  
+
+  render() {
+    return (
+      <form className="my-form">
+        <fieldset className="dados-pessoais">
+          <legend>Dados Pessoais</legend>
+          <label>Nome:
+            <input type="text" maxLength="40" required />
+          </label>
+          <label>Email:
+            <input type="text" maxLength="50" required />
+          </label>
+          <label>CPF:
+            <input type="text" maxLength="11" required />
+          </label>
+          <label>Endereço:
+            <input type="text" maxLength="200" required />
+          </label>
+          <label>Cidade:
+            <input type="text" maxLength="28" required />
+          </label>
+          <label>Estado:
+            <select name="estado" required>
+              {data.map((e, i) => (<option key={i}>{e}</option>))}
+            </select>
+          </label>
+          <p>
+            Tipo Residência:
+            <label>
+              Apartamaneto
+              <input type="radio" name="residence" value="ap" required />
+            </label>
+            <label>
+              Casa
+              <input type="radio" name="residence" value="casa" required />
+            </label>
+          </p>
+        </fieldset>
+        <fieldset className="dados-pessoais">
+          <legend>Último emprego</legend>
+          <label>
+            Resumo do currículo:
+            <textarea name="cResume" maxLength="1000" required></textarea>
+          </label>
+          <label>
+            Cargo:
+            <textarea name="cargo" maxLength="40" required></textarea>
+          </label>
+          <label>
+            Descrição do cargo:
+            <input type="text" name="cargo-desc" maxLength="500" required />
+          </label>
+        </fieldset>
+        <button type="submit">Enviar</button>
+      </form>
+    );
+  }
+}
+
+export default Form;

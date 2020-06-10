d613279af024c49451d0bf483eb59a1162e303c8
Rangel Paolo <paolorangel1996@outlook.com>
2020-06-10 02:32:25 -0300

    Salvando dog e excluindo.

--- exercises/13_2/life-cicle/src/App.js
+++ exercises/13_2/life-cicle/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import api from './api/api';
 
 class App extends Component {
   constructor(props) {
@@ -7,20 +6,34 @@ class App extends Component {
     this.state = {
       status: false,
       message: '',
+      nome: '',
     };
   }
 
+  setName = (e) => this.setState({ nome: e.target.value });
+
   componentDidMount = async () => {
-    await fetch('https://dog.ceo/api/breeds/image/random')
-      .then((resp) => resp.json())
-      .then((dogImage) => {
+    if (typeof(Storage) !== 'undefined') {
+      const dog = localStorage.getItem('DogData');
+      if (dog) {
+        const [ dogName, dogPic ] = [...JSON.parse(dog)];
         this.setState({
-          status: dogImage.status,
-          message: dogImage.message,
+          status: true,
+          message: dogPic,
+          nome: dogName,
         });
-        localStorage.dogImage = dogImage.message;
-        alert(dogImage.message.split('/')[4]);
-      });
+      } else {
+        await fetch('https://dog.ceo/api/breeds/image/random')
+          .then((resp) => resp.json())
+          .then((dogImage) => {
+            this.setState({
+              status: dogImage.status,
+              message: dogImage.message,
+            });
+            alert(dogImage.message.split('/')[4]);
+          });
+      }
+    }
   };
 
   shouldComponentUpdate = (nextProps, nextState) => {
@@ -28,11 +41,39 @@ class App extends Component {
     return true;
   };
 
+  salvaDog = () => {
+    const { message, nome } = this.state;
+    const dogData = [nome, message];
+    localStorage.setItem('DogData', JSON.stringify(dogData));
+    alert('Dog salvo');
+  };
+
+  doarDog = () => {
+    const dog = localStorage.getItem('DogData');
+    if (dog) localStorage.removeItem('DogData');
+    this.setState({
+      status: false,
+      message: '',
+    });
+    alert('Dog doado');
+  };
+
   render() {
-    const { status, message } = this.state;
+    const { status, message, nome } = this.state;
     return (
       <div>
         { status ? <img src={message} alt="A dog" height="300" /> : <div>Loading...</div> }
+        <div>
+          <h2>Nome: {nome}</h2>
+          <input
+            type="text"
+            value={nome}
+            onChange={this.setName}
+            placeholder="Digite o nome do Dog"
+          />
+          <button onClick={this.salvaDog}>Guardar dados do Dog</button>
+          <button onClick={this.doarDog}>Doar dog</button>
+        </div>
         <br/>
         <button onClick={this.componentDidMount}>Show another dog</button>
       </div>
--- exercises/13_2/life-cicle/src/api/api.js
+++
@@ -1,9 +0,0 @@
-const getDog = async () => {
-  let myDog = null;
-  await fetch('https://dog.ceo/api/breeds/image/random')
-    .then((resposta) => resposta.json())
-    .then((dog) => (myDog = dog.message));
-  return myDog;
-}
-
-export default getDog;

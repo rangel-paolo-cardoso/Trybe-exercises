0d3be78ff7946b073791c493e8b6469a825f6cd9
Rangel Paolo <paolorangel1996@outlook.com>
2020-07-04 21:41:14 -0300

    Somando o valor de todos os inputs com sucesso.

--- exercises/16_2/aula-16-2-ex2/src/actions/index.js
+++ exercises/16_2/aula-16-2-ex2/src/actions/index.js
@@ -1,1 +1,3 @@
-export const somaValor = value => ({ type: 'SOMA', value }); 
+export const somaValor1 = value => ({ type: 'SOMA1', value });
+export const somaValor2 = value => ({ type: 'SOMA2', value });
+export const somaValor3 = value => ({ type: 'SOMA3', value });
--- exercises/16_2/aula-16-2-ex2/src/componentes/Inputs.js
+++ exercises/16_2/aula-16-2-ex2/src/componentes/Inputs.js
@@ -1,42 +1,46 @@
 import React from 'react';
-import { somaValor } from '../actions';
+import { somaValor1, somaValor2, somaValor3 } from '../actions';
 import { connect } from 'react-redux';
 
-const Inputs = ({ calcular, counter }) => {
+const Inputs = ({ calcular1, calcular2, calcular3, somaInputUmReducer, somaInputDoisReducer, somaInputTresReducer }) => {
   return (
     <div>
       <p>
         <input
           id="field1"
           type="number"
-          onChange={(e) => calcular(e.target.value, e.target.id)}
+          onChange={(e) => calcular1(e.target.value, e.target.id)}
         />
       </p>
       <p>
         <input
           id="field2"
           type="number"
-          onChange={(e) => calcular(e.target.value, e.target.id)}
+          onChange={(e) => calcular2(e.target.value, e.target.id)}
         />
       </p>
       <p>
         <input
           id="field3"
           type="number"
-          onChange={(e) => calcular(e.target.value, e.target.id)}
+          onChange={(e) => calcular3(e.target.value, e.target.id)}
         />
       </p>
-      <p>Soma: {counter}</p>
+      <p>Soma: {somaInputUmReducer + somaInputDoisReducer + somaInputTresReducer}</p>
     </div>
   );
 };
 
 const mapDispatchToProps = dispatch => ({
-  calcular: valor => dispatch(somaValor(valor)),
+  calcular1: valor => dispatch(somaValor1(valor)),
+  calcular2: valor => dispatch(somaValor2(valor)),
+  calcular3: valor => dispatch(somaValor3(valor)),
 });
 
 const mapStateToProps = state => ({
-  counter: state,
+  somaInputUmReducer: state.somaInputUmReducer,
+  somaInputDoisReducer: state.somaInputDoisReducer,
+  somaInputTresReducer: state.somaInputTresReducer,
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Inputs);

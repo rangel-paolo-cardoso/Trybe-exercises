bb677eca7998534a8ca9df94d495921c735688db
Rangel Paolo <paolorangel1996@outlook.com>
2020-07-29 16:34:27 -0300

    Inicial segundo ex da part 2

---
+++ exercises/18_3/ex-02part2/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Greeting = ({ initialName = '' }) => {
+  // 🐨 initialize the state to the value from localStorage
+  // 💰 window.localStorage.getItem('name') || initialName
+  const [name, setName] = React.useState(initialName);
+
+  // 🐨 Here's where you'll use `React.useEffect`.
+  // The callback should set the `name` in localStorage.
+  // 💰 window.localStorage.setItem('name', name)
+
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+  return (
+    <div>
+      <form>
+        <label htmlFor="name">Name: </label>
+        <input onChange={handleChange} id="name" />
+      </form>
+      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+    </div>
+  );
+};
+
+const App = () => <Greeting />;
+
+export default App;

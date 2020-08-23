4047478f3c2a6577175842fae1269876d3779a67
Rangel Paolo <paolorangel1996@outlook.com>
2020-08-23 19:28:31 -0300

    Rodada 5, exercicio 3.9 feito.

---
+++ exercises/21_2/using_DB3-3-9.sql
@@ -0,0 +1,8 @@
+SELECT
+  Warehouses.Code AS 'Warehouse Code'
+FROM praticando.Warehouses AS Warehouses
+WHERE Warehouses.Capacity < (
+    SELECT COUNT(*)
+    FROM praticando.Boxes
+    WHERE Warehouses.Code = Boxes.Warehouse
+);

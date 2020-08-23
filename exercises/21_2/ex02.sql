eb085ccfd44c0a9ef5996e5ad530b5f95cf0ddfa
Rangel Paolo <paolorangel1996@outlook.com>
2020-08-23 02:57:38 -0300

    Exercício 3 da atividade 2 feito.

--- exercises/21_2/ex02.sql
+++ exercises/21_2/ex02.sql
@@ -1,3 +1,10 @@
 SELECT DISTINCT building FROM employees;
 
 SELECT Building_name, Capacity FROM Buildings;
+
+SELECT
+  DISTINCT Building_name,
+  Role
+FROM Buildings
+LEFT JOIN Employees
+ON Building_name = Building;

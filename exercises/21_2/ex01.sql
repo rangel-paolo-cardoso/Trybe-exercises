a44c4c079ee60905a04ae79234e210cdc56c5efb
Rangel Paolo <paolorangel1996@outlook.com>
2020-08-23 02:23:54 -0300

    Exercício 3 da atividade 1 feito.

--- exercises/21_2/ex01.sql
+++ exercises/21_2/ex01.sql
@@ -15,3 +15,9 @@ JOIN Boxoffice AS B
 ON M.id = B.movie_id
 WHERE B.international_sales > domestic_sales;
 
+SELECT
+  M.title
+FROM Movies AS M
+JOIN Boxoffice AS B
+ON M.id = B.movie_id
+ORDER BY B.rating DESC;

24f9f416fd190347633ca2f2b04d69df98a10bb7
Rangel Paolo <paolorangel1996@outlook.com>
2020-08-23 02:21:42 -0300

    exercício 2 da atividade 1 feito

--- exercises/21_2/ex01.sql
+++ exercises/21_2/ex01.sql
@@ -5,3 +5,13 @@ SELECT
 FROM Movies AS M
 JOIN Boxoffice AS B
 ON M.id = B.movie_id;
+
+SELECT
+  M.title,
+  B.domestic_sales,
+  B.international_sales
+FROM Movies AS M
+JOIN Boxoffice AS B
+ON M.id = B.movie_id
+WHERE B.international_sales > domestic_sales;
+

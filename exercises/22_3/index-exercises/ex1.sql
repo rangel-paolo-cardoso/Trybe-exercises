fff176576c22785d54c9af900c3e7589fbf7a136
Rangel Paolo <paolorangel1996@outlook.com>
2020-08-31 19:02:48 -0300

    index exercício 2 concluído.

--- exercises/22_3/index-exercises/ex1.sql
+++ exercises/22_3/index-exercises/ex1.sql
@@ -1,11 +1,11 @@
 USE sakila;
-CREATE FULLTEXT INDEX index_category ON sakila.category(name);
+CREATE FULLTEXT INDEX index_category ON category(name);
 
 -- Query cost: 0.35
 SELECT * FROM category
 WHERE MATCH(name) AGAINST('action');
 
-DROP INDEX index_category ON sakila.category;
+DROP INDEX index_category ON category;
 
 -- Query cost: 1.85
 SELECT * FROM category

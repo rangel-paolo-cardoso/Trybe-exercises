b3a436d83286741912b7b06e71ec5dc8a3d241a1
Rangel Paolo <paolorangel1996@outlook.com>
2020-09-21 15:21:02 -0300

    Exercicio 3 concluido.

--- exercises/26_1/index.js
+++ exercises/26_1/index.js
@@ -1,21 +1,45 @@
-const leEntrada = require('readline-sync');
+const inquirer = require('inquirer');
 
-function resultIMC (imc) {
-    if (imc < 17) return 'Muito abaixo do peso';
-    if (imc >= 17 && imc < 18.5) return 'Abaixo do peso (magreza)';
-    if (imc >= 18.5 && imc < 25) return 'Peso normal';
-    if (imc >= 25 && imc < 30) return 'Acima do peso (sobrepeso)';
-    if (imc >= 30 && imc < 35) return 'Obesidade I';
-    if (imc >= 35 && imc < 40) return 'Obesidade II (severa)';
-    return 'Obesidade III e IV (mórbida)';
+function validateEntry (input) {
+  return !isNaN(parseFloat(input)) || 'Por favor, digite um número válido';
 }
 
-function calculaIMC () {
-  const peso = leEntrada.questionFloat('Digite seu peso (Kg) ');
-  const altura = leEntrada.questionFloat('Digite sua altura (Cm) ');
+function resultIMC (imc) {
+  if (imc < 17) return 'Muito abaixo do peso';
+  if (imc >= 17 && imc < 18.5) return 'Abaixo do peso (magreza)';
+  if (imc >= 18.5 && imc < 25) return 'Peso normal';
+  if (imc >= 25 && imc < 30) return 'Acima do peso (sobrepeso)';
+  if (imc >= 30 && imc < 35) return 'Obesidade I';
+  if (imc >= 35 && imc < 40) return 'Obesidade II (severa)';
+  return 'Obesidade III e IV (mórbida)';
+}
+
+async function calculaIMC () {
+  const respostas = await inquirer.prompt([
+    {
+      name: 'peso',
+      type: 'input',
+      message: 'Digite seu peso (Kg)? ',
+      validate: validateEntry
+    },
+    {
+      name: 'altura',
+      type: 'input',
+      message: 'Digite sua altura (m)? ',
+      validate: validateEntry
+    }
+  ]);
+
+  const peso = parseFloat(respostas.peso);
+  const altura = parseFloat(respostas.altura);
+
   const imc = peso / Math.pow(altura, 2);
+
+  console.log('Seu peso: %f', peso);
+  console.log('Sua altura: %f', altura);
   console.log('Seu IMC é: %s', imc.toFixed(2));
-  return resultIMC(imc);
+  console.log(resultIMC(imc));
+  return;
 }
 
 console.log(calculaIMC());

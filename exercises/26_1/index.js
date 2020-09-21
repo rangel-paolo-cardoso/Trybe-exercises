843d234cd366bb29802399f29fd3e9c477675ff4
Rangel Paolo <paolorangel1996@outlook.com>
2020-09-21 03:49:46 -0300

    Exercício 2 concluído.

--- exercises/26_1/index.js
+++ exercises/26_1/index.js
@@ -1,30 +1,21 @@
 const leEntrada = require('readline-sync');
 
-// function resultIMC (imc) {
-//   switch (imc) {
-//     case (imc < 17):
-//       return 'Muito abaixo do peso';
-//     case (imc >= 17 && imc < 18.5):
-//       return 'Abaixo do peso';
-//     case (imc >= 18.5 && imc < 25):
-//       return 'Peso normal';
-//     case (imc >= 25 && imc < 30):
-//       return 'Acima do peso';
-//     case (imc >= 30 && imc < 35):
-//       return 'Obesidade I';
-//     case (imc >= 35 && imc < 40):
-//       return 'Obesidade II (severa)';
-//     default:
-//       return 'Obesidade III (mórbida)';
-//   }
-// }
+function resultIMC (imc) {
+    if (imc < 17) return 'Muito abaixo do peso';
+    if (imc >= 17 && imc < 18.5) return 'Abaixo do peso (magreza)';
+    if (imc >= 18.5 && imc < 25) return 'Peso normal';
+    if (imc >= 25 && imc < 30) return 'Acima do peso (sobrepeso)';
+    if (imc >= 30 && imc < 35) return 'Obesidade I';
+    if (imc >= 35 && imc < 40) return 'Obesidade II (severa)';
+    return 'Obesidade III e IV (mórbida)';
+}
 
 function calculaIMC () {
   const peso = leEntrada.questionFloat('Digite seu peso (Kg) ');
   const altura = leEntrada.questionFloat('Digite sua altura (Cm) ');
   const imc = peso / Math.pow(altura, 2);
-  // return resultIMC(imc);
-  return imc.toFixed(2);
+  console.log('Seu IMC é: %s', imc.toFixed(2));
+  return resultIMC(imc);
 }
 
 console.log(calculaIMC());

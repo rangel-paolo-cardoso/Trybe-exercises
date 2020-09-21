489aa1737d1bdd2b1c6fbacdc4453b6cb1556ed6
Rangel Paolo <paolorangel1996@outlook.com>
2020-09-21 02:11:58 -0300

    Bonus2 exercicio 16 feito com algo mais.

--- exercises/21_2/bonus_2DB/exercises/bonus2ex16.sql
+++ exercises/21_2/bonus_2DB/exercises/bonus2ex16.sql
@@ -1,2 +1,5 @@
 INSERT INTO Departments (Code, Name, Budget)
 VALUES (11, 'Quality Assurance', 40000);
+
+INSERT INTO Employees (SSN, Name, LastName, Department)
+VALUES (847219811, 'Mary', 'Moore', 11);

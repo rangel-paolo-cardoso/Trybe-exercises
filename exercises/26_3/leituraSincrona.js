053610845b91d807e9fa299d2398ff908631c1ab
Rangel Paolo <paolorangel1996@outlook.com>
2020-09-23 20:20:08 -0300

    Formatado forma de exibir tempo.

--- exercises/26_3/leituraSincrona.js
+++ exercises/26_3/leituraSincrona.js
@@ -3,12 +3,11 @@ const fs = require('fs');
 const inicioTest = Date.now();
 const teste = fs.readFileSync('./teste.txt');
 
-const tempoGasto = Date.now() - inicioTest; // Diferença entre horários.
-
 function leArquivoDeFormaSincrona () {
   console.log(teste.toString('utf8')); // exebe o conteúdo do arquivo.
+  const tempoGasto = Date.now() - inicioTest; // Diferença entre horários.
   console.log(`teste.txt: lidos ${teste.byteLength} bytes`); // exibe tamanho em bytes.
-  console.log(`Tempo gasto na execução: ${tempoGasto} segundos.`); // Tempo gasto.
+  console.log(`Tempo gasto na execução: ${parseInt(tempoGasto / 1000, 10)} segundos.`); // Tempo gasto.
   return;
 }
 

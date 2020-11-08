40ee7f051a9a9a673ca3c62299d58625aef43964
Rangel Paolo <paolorangel1996@outlook.com>
2020-11-07 23:55:13 -0300

    Simulando erro com sucesso

--- exercises/29_2/index.js
+++ exercises/29_2/index.js
@@ -3,9 +3,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) =>
-  res.send(`Exercício de PM2! Ambiente de: ${process.env.ENVIRONMENT}`)
-);
+app.get('/', (req, res) => {
+  res.send('Vai ocorrer um erro...');
+  process.exit(1);
+});
 
 app.listen(port, () =>
   console.log(`Example app listening on port port!`)

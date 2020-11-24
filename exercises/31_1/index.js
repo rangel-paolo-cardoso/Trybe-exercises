da0ae18c2fe244919e9b0af76dc6c5216ca18b0f
Rangel Paolo <paolorangel1996@outlook.com>
2020-11-23 23:01:52 -0300

    Rotas criadas

--- exercises/31_1/index.js
+++ exercises/31_1/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const plantsAPI = require('./plants');
 
 const app = express();
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/plants', (req, res) => {});
+
+app.get('/plant/:id', (req, res) => {});
+
+app.delete('/plant/:id', (req, res) => {});
+
+app.post('/plant/:id', (req, res) => {});
+
+app.post('/plant', (req, res) => {});
+
+app.get('/sunny/:id', (req, res) => {});
+
 
 app.listen(3000, () => console.log('Aplicação rodando!'));

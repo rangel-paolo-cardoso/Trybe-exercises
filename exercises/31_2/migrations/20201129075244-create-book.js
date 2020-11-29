0f5740759dabdbfaed5e4e15247e4c8956ff7b0c
Rangel Paolo <paolorangel1996@outlook.com>
2020-11-29 05:03:15 -0300

    Primeiro Sequelize feito

---
+++ exercises/31_2/migrations/20201129075244-create-book.js
@@ -0,0 +1,32 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) =>
+    queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      author: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      pageQuantity: {
+        allowNull: true,
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      }
+    }),
+
+  down: async (queryInterface) =>
+    queryInterface.dropTable('Books'),
+};

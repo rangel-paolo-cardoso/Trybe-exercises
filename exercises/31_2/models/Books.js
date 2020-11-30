5d9e3c7be5ab3a63430b9378737b9cd13156a42f
Rangel Paolo <paolorangel1996@outlook.com>
2020-11-29 23:24:08 -0300

    Model criado

--- exercises/31_2/models/Books.js
+++ exercises/31_2/models/Books.js
@@ -1,1 +1,10 @@
+const Book = (sequelize, DataTypes) => 
+  sequelize.define('Book', {
+    title: DataTypes.STRING,
+    author: DataTypes.STRING,
+    pageQuantity: DataTypes.INTEGER,
+  });
 
+module.exports = {
+  Book,
+};

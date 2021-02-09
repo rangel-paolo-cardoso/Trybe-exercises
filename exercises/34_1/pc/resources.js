66e1b239515a58cb9d222123b8d9f4d776f562a3
Rangel Paolo <paolorangel1996@outlook.com>
2021-02-09 12:43:35 -0300

    Showing memory info

--- exercises/34_1/pc/resources.js
+++ exercises/34_1/pc/resources.js
@@ -10,6 +10,6 @@ console.log('Cores: ', cores.length);
 cores.forEach((core, index) => {
   console.log(`Core${index + 1} - Modelo: ${core.model} - Speed: ${core.speed / 1000}Ghz`);
 });
-console.log('Versão: ', so.release());
-console.log('Arquitetura: ', so.arch());
+
+console.log(`Memória disponível: ${so.totalmem() / Math.pow(1024, 3)}GB`);
 

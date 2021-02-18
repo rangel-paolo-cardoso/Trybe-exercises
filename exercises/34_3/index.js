6693fc67c36eef6948eb7990f871b793f8aecd52
Rangel Paolo <paolorangel1996@outlook.com>
2021-02-18 02:38:47 -0300

    Ex 4 done

--- exercises/34_3/index.js
+++ exercises/34_3/index.js
@@ -19,6 +19,7 @@
 // Exercício 4 :
 //   Bloqueie agora o tráfego de saída para HTTP. Lembre-se, também é utilizado o protocolo tcp e a porta 80.
 //   Para testar sua regra, tente acessar um site pelo navegador que use HTTP .
+// Possible answer: sudo iptables -A OUTPUT -p tcp --sport 80 -j REJECT
 
 // Exercício 5 :
 //   Para finalizar, vamos limpar todas as regras. Para isso, utilize o comando --flush do iptables (Linux).

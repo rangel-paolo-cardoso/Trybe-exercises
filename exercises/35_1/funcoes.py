e2d361e75b280c4d2e5821b4637100f06212de07
Rangel Paolo <paolorangel1996@outlook.com>
2021-03-11 18:46:56 -0300

    Exercise 2 solved

--- exercises/35_1/funcoes.py
+++ exercises/35_1/funcoes.py
@@ -3,3 +3,10 @@ def maior_numero(number_1, number_2):
     if number_1 > number_2:
         return number_1
     return number_2
+
+
+def arithmetic_average(numbers):
+    sum = 0
+    for number in numbers:
+        sum += number
+    return sum / len(numbers)

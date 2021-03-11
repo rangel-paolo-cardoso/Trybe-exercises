c53690ed3201c27e60294562263ee1156d35dfc7
Rangel Paolo <paolorangel1996@outlook.com>
2021-03-11 18:52:11 -0300

    Exercise 3 solved

--- exercises/35_1/funcoes.py
+++ exercises/35_1/funcoes.py
@@ -1,12 +1,21 @@
-# exercício 1
+# Exercise 1
 def maior_numero(number_1, number_2):
     if number_1 > number_2:
         return number_1
     return number_2
 
 
+# Exercise 2
 def arithmetic_average(numbers):
     sum = 0
     for number in numbers:
         sum += number
     return sum / len(numbers)
+
+
+# Exercise 3
+def asterisk_square(n):
+    for row in range(5):
+        for column in range(5):
+            print('*', end='')
+        print()

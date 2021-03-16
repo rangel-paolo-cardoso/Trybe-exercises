af121264b4eb462770998899a0dff8b9fbfbcfa2
Rangel Paolo <paolorangel1996@outlook.com>
2021-03-16 20:18:05 -0300

    Exercise 2 done

--- exercises/35_2/exercises.py
+++ exercises/35_2/exercises.py
@@ -1,3 +1,6 @@
+import random
+
+
 # exercise 1
 def vertical_name_stairs():
     name = input('Type your name: ')
@@ -7,4 +10,25 @@ def vertical_name_stairs():
         print()
 
 
-print(vertical_name_stairs())
+WORD_LIST = ['macaco', 'cinema', 'groselha']
+
+
+def show_scrambled_word():
+    secret_word = random.choice(WORD_LIST)
+    scrambled_word = ''.join(random.sample(secret_word, len(secret_word)))
+    print('What is this word?', scrambled_word)
+
+
+# exercise 2
+def guess_the_word():
+    attempts = 3
+    show_scrambled_word()
+    while attempts > 0:
+        choice = input('Type your guess: ')
+        if choice in WORD_LIST:
+            return 'You won! :-)'
+        attempts -= 1
+    return 'You lose'
+
+
+print(guess_the_word())

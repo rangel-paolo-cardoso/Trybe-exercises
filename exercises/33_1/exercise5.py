aa7816a4fd75ea574d81b72a6fc8942b0d41f7d3
Rangel Paolo <paolorangel1996@outlook.com>
2021-10-30 02:29:36 -0300

    Exercise 5 enhanced

--- exercises/33_1/exercise5.py
+++ exercises/33_1/exercise5.py
@@ -5,11 +5,9 @@ def painting_cost(square_meters):
     can_liters = 18
     can_cost_reais = 80
     paint_amoun_liters = square_meters // 3
-    can_amount = 0
+    can_amount = 1
     if paint_amoun_liters > can_liters:
         can_amount = ceil(paint_amoun_liters / can_liters)
-    else:
-        can_amount = 1
     return (can_amount, can_amount * can_cost_reais)
 
 

a969b0bafa0b725ed3f724f8b2aad3d9f7c6a8d0
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2024-04-18 18:55:18 -0300

    Exercise 5 of Learning Python - done

--- ciencia-da-computacao/learning_python/exercises.py
+++ ciencia-da-computacao/learning_python/exercises.py
@@ -1,3 +1,6 @@
+from math import ceil
+
+
 # Exercise 1
 def largest_number(number_1, number_2):
     return number_1 if number_1 > number_2 else number_2
@@ -22,6 +25,7 @@ print(f"List average 2: {arithmeric_average(list2)}")  # Expected = 11.6...
 print()
 
 
+# Exercise 3
 def asteristic_square(n):
     for number in range(n):
         print("*" * n)
@@ -32,6 +36,7 @@ asteristic_square(5)
 print()
 
 
+# Exercise 4
 def longest_name(name_list):
     big_name = name_list[0]
     for name in name_list:
@@ -43,4 +48,22 @@ def longest_name(name_list):
 print("=" * 5, "Exercise 4", "=" * 5)
 name_list = ["José", "Lucas", "Nádia", "Fernanda", "Cairo", "Joana"]
 print(longest_name(name_list))
+print()
+
+
+# Exercise 5
+def calculate_amount_of_paint(wall_size):
+    can_volume = 18
+    can_price = 80.0
+    can_quantity = ceil(wall_size / (can_volume * 3))
+    total_price = can_quantity * can_price
+    return (can_quantity, total_price)
+
+
+print("=" * 5, "Exercise 5", "=" * 5)
+wall_size = int(input("Enter the wall size in square meters: "))
+print(
+    f"The amount of paint need for a wall of {wall_size}",
+    f"square meter is {calculate_amount_of_paint(wall_size)}"
+)
 print()

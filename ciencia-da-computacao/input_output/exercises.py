4f7068207d6bdd42f698b42b1ced14b04198b130
Rangel Paolo Cardoso Bomfim <paolorangel1996@outlook.com>
2024-09-02 01:45:29 -0300

    Exercise 2 of Data Input and Output - done

--- ciencia-da-computacao/input_output/exercises.py
+++ ciencia-da-computacao/input_output/exercises.py
@@ -1,15 +1,36 @@
+from random import sample
+from random import choice
+
 # Exercise 1
 def inverted_name(name):
-    name_length = len(name)
-    # print("name_length", name_length)
-    for row in range(0, name_length):
-        # print("name_length", name_length)
-        for letter in range(name_length, name_length - 1, -1):
-            print(name[:name_length])
-        name_length -= 1
+    for removed_letters in range(len(name)):
+        for index in range(len(name) - removed_letters):
+            print(name[index], end="")
+        print()
 
 
 print("=" * 5, "Exercise 1", "=" * 5)
-print(f"Inverted name: {inverted_name('PEDRO')}")
 print(f"Inverted name: {inverted_name('RANGEL')}")
+print()
+
+
+# Exercise 2
+def scrambled_word_game():
+    word = choice(["water", "towel", "computer", "driver", "cable"])
+    scrambled_word = "".join(sample(word, len(word)))
+    attempts = 0
+    won = False
+    while True:
+        print(f"The scrambled word is: {scrambled_word}")
+        guess = input("Enter your guess: ")
+        won = True if guess == word else False
+        attempts += 1
+        if won or attempts == 3:
+            break
+    print(f"The scrambled word was {scrambled_word}")
+    print(f"You {'Won' if won else 'Lose'}")
+
+
+print("=" * 5, "Exercise 2", "=" * 5)
+scrambled_word_game()
 print()
